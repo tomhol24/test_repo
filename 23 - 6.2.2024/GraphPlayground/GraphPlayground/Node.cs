151aa05ecbc869b8442f053d1bf109a41722cedb
tomhol24 <holtomi@seznam.cz>
2024-03-05 10:59:43 +0100

    aktualizace_repozitare

---
+++ 23 - 6.2.2024/GraphPlayground/GraphPlayground/Node.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace GraphPlayground
+{
+    //Simple node class, no comments needed
+    internal class Node
+    {
+        public int index;
+
+        public List<Node> neighbors;
+
+        public bool visited; //jestli jsem jej navštívil nebo ne
+        
+        public Node cameFrom; //To si pamatuje odkud jsem do daného uzlu přišel, jen uložím uzel ze kterého jsem se dostal do toho aktuálního
+
+        public Node(int index)
+        {
+            this.index = index;
+            neighbors = new List<Node>();
+            visited = false;
+            cameFrom = null; //Počáteční nastavení na nulu
+        }
+    }
+}
---
+++ 23 - 6.2.2024/GraphPlayground/GraphPlayground/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace GraphPlayground
+{
+    internal class Program
+    {
+        public static void DFS(Graph graph, Node startNode, Node targetNode = null)
+        {
+
+        }
+
+        public static void BFS(Graph graph, Node startNode, Node targetNode = null) //Pro zobrazení grafu https://csacademy.com/app/graph_editor/
+        {
+            startNode.visited = true;
+            Node currentNode = startNode; //nastavím si jej jako startovní protože v něm začínám
+            while (true) 
+            {
+                Console.WriteLine($"Aktuálně jsem v uzlu " + currentNode.index);
+                Node haveNeighborToVisit = null;
+                foreach (Node neighbor in currentNode.neighbors) 
+                { 
+                    if (!neighbor.visited) 
+                    { 
+                        haveNeighborToVisit = neighbor;
+                        break;  
+                    
+                    }             
+                }
+                if (haveNeighborToVisit == null) //Všichni sousedi navšítvení //pokud se to nikdy nedostalo do toho ifu nahoře (dokončené iterace a všichni sousedi jsou navšítvení) tak dávám return a končím celé DFS protože už není kam jít
+                {
+                    if (currentNode == startNode) //Na to abych to skončil úplně musím být zpátky na začátku
+                    {
+                        Console.WriteLine("Jsem v startovacím uzlu, nemám kam jít, končím DFS.");
+                        return;
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Jsem ve slepé uličce, vracím se z uzlu " + currentNode.index + " do uzlu " + currentNode.cameFrom); 
+                        currentNode = currentNode.cameFrom;
+                    }
+                }
+                else //Mám nenavšíteného souseda, kterého jdu navštívit
+                {
+                    Console.WriteLine("Jdu z uzlu " + currentNode.index + " do nenavštíveného souseda " + haveNeighborToVisit.index);
+                    haveNeighborToVisit.visited = true;
+                    haveNeighborToVisit.cameFrom = currentNode;
+                    currentNode = haveNeighborToVisit;
+                }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            //Create and print the graph
+            Graph graph = new Graph();
+            graph.PrintGraph();
+            graph.PrintGraphForVisualization();
+
+            //Call both algorithms with a random starting node
+            Random rng = new Random();
+            DFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]); //zaviólá se a dá se tomu náhodný startovací node
+            BFS(graph, graph.nodes[rng.Next(0, graph.nodes.Count)]);
+
+            Console.ReadKey();
+        }
+    }
+}
